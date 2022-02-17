8ce7251d9aa1555abee83c37ba78246c5ec36964
EricDeWald <eric.l.dewald@gmail.com>
2022-02-16 18:21:37 -0600

    Update to controllers
    created index.js for models and outline moved to controllers.

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const { User, Thought } = require('../models');
+// GET all users
+
+
+// GET a single user by its _id and populated thought and friend data
+
+// POST a new user:
+
+// // example data
+// {
+//   "username": "lernantino",
+//   "email": "lernantino@gmail.com"
+// }
+// PUT to update a user by its _id
+
+// DELETE to remove user by its _id
+
+// BONUS: Remove a user's associated thoughts when deleted.
+
+// delete many find user id to delete the thoughts with that user id
+
+
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
